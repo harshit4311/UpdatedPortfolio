4a67bdafbbc441d222f9a4506864baa17e07c18e
Harshit <harshit.23bcs10046@ms.sst.scaler.com>
2024-06-03 09:55:03 +0530

    change resume path

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "/Users/harshit/Portfolio-HarshitShah/src/Harshit Shah-UpdatedResume copy.pdf";
+import pdf from "/Users/harshit/Portfolio-HarshitShah/src/Assets/Harshit Shah-UpdatedResume copy.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
